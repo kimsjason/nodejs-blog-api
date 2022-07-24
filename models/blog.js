edf8c00df4867b3eff4f652d03df362550b6f098
Jason Kim <jason.s.kim95@gmail.com>
2022-07-24 19:18:45 -0400

    Add blog post model

---
+++ models/blog.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const BlogSchema = new Schema(
+  {
+    title: { type: String, required: true },
+    text: { type: String, required: true },
+    author: { type: String, required: true },
+    published: { type: Boolean, required: true },
+    comments: [{ type: Schema.Types.ObjectId, ref: "Comment", required: true }],
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Blog", BlogSchema);
