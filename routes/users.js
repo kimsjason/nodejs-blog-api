a50d12e45d1fbc902822e2cb66b0b5d525f8bcf7
Jason Kim <jason.s.kim95@gmail.com>
2022-07-25 15:55:09 -0400

    Add user routes

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,20 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
+const userController = require("../controllers/userController");
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+/* GET - read all users. */
+router.get("/", userController.users);
+
+/* POST - create new user */
+router.post("/user", userController.user_post);
+
+/* GET - read user. */
+router.get("/user/:id", userController.user_get);
+
+/* PUT - update user. */
+router.put("/user/:id", userController.user_put);
+
+/* DELETE - delete user. */
+router.delete("/user/:id", userController.user_delete);
 
 module.exports = router;
