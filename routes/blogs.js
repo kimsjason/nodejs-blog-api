a330309d310306d85497ffa652af3d15bff9b69a
Jason Kim <jason.s.kim95@gmail.com>
2022-07-26 13:55:38 -0400

    Add blog comment routes

--- routes/blogs.js
+++ routes/blogs.js
@@ -2,6 +2,7 @@ var express = require("express");
 var router = express.Router();
 const blogController = require("../controllers/blogController");
 
+/* ---------- BLOG ROUTES ---------- */
 /* GET - read all blogs. */
 router.get("/", blogController.blogs);
 
@@ -16,5 +17,21 @@ router.put("/blog/:id", blogController.blog_put);
 
 /* DELETE - delete blog. */
 router.delete("/blog/:id", blogController.blog_delete);
+
+/* ---------- BLOG COMMENT ROUTES ---------- */
+/* GET - read all comments. */
+router.get("/blog/:id/comments", blogController.comments);
+
+/* POST - create new comment */
+router.post("/blog/:id/comment", blogController.comment_post);
+
+/* GET - read comment. */
+router.get("/blog/:id1/comment/:id2", blogController.comment_get);
+
+/* PUT - update comment. */
+router.put("/blog/:id1/comment/:id2", blogController.comment_put);
+
+/* DELETE - delete comment. */
+router.delete("/blog/:id1/comment/:id2", blogController.comment_delete);
 
 module.exports = router;
