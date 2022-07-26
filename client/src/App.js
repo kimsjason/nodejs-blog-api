7b3287fbc29c18813a1bd6283835bbc3e34b9d22
Jason Kim <jason.s.kim95@gmail.com>
2022-07-26 16:49:34 -0400

    Fetch users from database

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,24 @@
+import { useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
-  return <div></div>;
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    // fetch users from database
+    const fetchUsers = async () => {
+      await fetch("http://localhost:9000/users")
+        .then((res) => res.json())
+        .then((res) => {
+          const users = res.users;
+          setUsers(users);
+        })
+        .catch((err) => console.error("Error: ", err));
+    };
+    fetchUsers();
+  }, []);
+
+  return <div className="App"></div>;
 }
 
 export default App;
