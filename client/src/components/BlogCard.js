d65bf1f1154f5193bdcbbcbe6012f8392946eecd
Jason Kim <jason.s.kim95@gmail.com>
2022-08-02 19:53:45 -0400

    Add blog card component

---
+++ client/src/components/BlogCard.js
@@ -0,0 +1,28 @@
+import { useNavigate } from "react-router-dom";
+import "../styles/BlogCard.css";
+
+const BlogCard = ({ blog }) => {
+  const navigate = useNavigate();
+
+  return (
+    <div
+      className="blog-card"
+      onClick={() => navigate(`/blogs/blog/${blog._id}`)}
+    >
+      <img src={require("../assets/javascript.png")} alt="blog img" />
+      <div className="info">
+        <div className="created-at">
+          {new Date(blog.createdAt).toLocaleDateString()}
+        </div>
+        <div className="title">{blog.title}</div>
+        <div className="author">by {blog.author}</div>
+        <div className="text">{blog.text.slice(0, 250)}...</div>
+        <a href={`/blogs/blog/${blog._id}`} className="read-more">
+          Read More
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default BlogCard;
