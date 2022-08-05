2226221e2f2841ab0a00de67b86e796f40d68a62
Jason Kim <jason.s.kim95@gmail.com>
2022-08-04 23:00:24 -0400

    Update navigation view for logged in/out

--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -1,7 +1,25 @@
-import "../styles/Nav.css";
 import { ReactComponent as Logo } from "../assets/cubes-solid.svg";
+import "../styles/Nav.css";
 
 const Nav = () => {
+  // const navigate = useNavigate();
+  const isAuthenticated = JSON.parse(localStorage.getItem("isAuthenticated"));
+
+  const logout = () => {
+    fetch("http://localhost:9000/logout", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((res) => res.json())
+      .then((res) => {
+        localStorage.setItem("isAuthenticated", res.isAuthenticated);
+        // redirect to home page
+        window.location.href = "http://localhost:3000/";
+      });
+  };
+
   return (
     <div>
       <nav>
@@ -18,14 +36,30 @@ const Nav = () => {
             <a href="/bloggers">Bloggers</a>
           </li>
         </ul>
-        <ul className="site-access">
-          <li className="login">
-            <a href="/login">Log In</a>
-          </li>
-          <li className="signup">
-            <a href="/signup">Sign Up</a>
-          </li>
-        </ul>
+        {isAuthenticated ? (
+          <ul className="your-account">
+            <li className="create-blog">
+              <a href="/blogs/create-blog">Create Blog</a>
+            </li>
+            <li className="your-blogs">
+              <a href="/blogs/create-blog">My Blogs</a>
+            </li>
+            <li className="logout">
+              <a href="#" onClick={logout}>
+                Log Out
+              </a>
+            </li>
+          </ul>
+        ) : (
+          <ul className="site-access">
+            <li className="login">
+              <a href="/login">Log In</a>
+            </li>
+            <li className="signup">
+              <a href="/signup">Sign Up</a>
+            </li>
+          </ul>
+        )}
       </nav>
     </div>
   );
