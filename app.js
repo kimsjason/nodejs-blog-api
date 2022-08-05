53bfeb6720a5c197ad54bef979b34e55ee2c11bc
Jason Kim <jason.s.kim95@gmail.com>
2022-08-04 22:54:20 -0400

    Initialize passport middleware with local strategy and session persistence

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 var createError = require("http-errors");
 var express = require("express");
 var path = require("path");
+const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
+const session = require("express-session");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 var cors = require("cors");
@@ -11,12 +14,54 @@ const usersRouter = require("./routes/users");
 const blogsRouter = require("./routes/blogs");
 
 const db = require("./config/config");
+const User = require("./models/user");
 
 var app = express();
 
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "pug");
+
+// passport middleware
+passport.use(
+  new LocalStrategy((username, password, done) => {
+    User.findOne({ username: username }).exec((err, user) => {
+      if (err) {
+        return done(err);
+      }
+
+      if (!user) {
+        return done(null, false, { message: "Incorrect username" });
+      }
+
+      if (user.password !== password) {
+        return done(null, false, { message: "Incorrect password" });
+      }
+
+      return done(null, user);
+    });
+  })
+);
+
+passport.serializeUser(function (user, done) {
+  done(null, user.id);
+});
+
+passport.deserializeUser(function (id, done) {
+  User.findById(id, function (err, user) {
+    done(err, user);
+  });
+});
+
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true,
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.use(cors());
 app.use(logger("dev"));
