19bf3acc02f181fbb81eeff4cc1e6056089101b5
Jason Kim <jason.s.kim95@gmail.com>
2022-08-04 22:47:14 -0400

    Add login/logout post routes

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
+const loginController = require("../controllers/loginController");
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+/* POST - login page. */
+router.post("/login", loginController.login_post);
+
+/* POST - logout page. */
+router.post("/logout", loginController.logout_post);
 
 module.exports = router;
