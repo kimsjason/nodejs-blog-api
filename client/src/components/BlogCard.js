bb5d4f64e942d4ff1eaf9041b0265e783f7d1af2
Jason Kim <jason.s.kim95@gmail.com>
2022-08-09 21:21:46 -0400

    Add ability to delete blogs

--- client/src/components/BlogCard.js
+++ client/src/components/BlogCard.js
@@ -1,8 +1,19 @@
+import { useContext } from "react";
 import { useNavigate } from "react-router-dom";
+import { authContext } from "../contexts/AuthContext";
+import { ReactComponent as DeleteIcon } from "../assets/delete.svg";
 import "../styles/BlogCard.css";
 
 const BlogCard = ({ blog }) => {
+  const { auth } = useContext(authContext);
+
   const navigate = useNavigate();
+
+  const deleteBlog = (blog) => {
+    fetch(`http://localhost:9000/blogs/blog/${blog}`, {
+      method: "DELETE",
+    }).catch((err) => console.err(err));
+  };
 
   return (
     <div
@@ -11,6 +22,18 @@ const BlogCard = ({ blog }) => {
     >
       <img src={require("../assets/javascript.png")} alt="blog img" />
       <div className="info">
+        {auth.data.user && auth.data.user._id === blog.author._id ? (
+          <DeleteIcon
+            className="delete-blog"
+            onClick={(event) => {
+              event.stopPropagation();
+              deleteBlog(blog._id);
+              navigate(0, {
+                state: { blogType: blog.published ? "published" : "drafts" },
+              });
+            }}
+          />
+        ) : null}
         <div className="created-at">
           {new Date(blog.createdAt).toLocaleDateString()}
         </div>
@@ -18,7 +41,7 @@ const BlogCard = ({ blog }) => {
         <div className="author">
           by {`${blog.author.firstName} ${blog.author.lastName}`}
         </div>
-        <div className="text">{blog.text.slice(0, 250)}...</div>
+        <div className="text">{blog.text.slice(0, 100)}...</div>
         <a href={`/blogs/blog/${blog._id}`} className="read-more">
           Read More
         </a>
