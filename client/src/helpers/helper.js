98bca954adb2c9e263df3e3b89d53609494958fa
Jason Kim <jason.s.kim95@gmail.com>
2022-08-11 16:32:35 -0400

    Add helper function to return time elapsed in human readable format using  luxon

---
+++ client/src/helpers/helper.js
@@ -0,0 +1,32 @@
+import { DateTime } from "luxon";
+
+export const getTimeAgo = (isoDate) => {
+  const oldTime = DateTime.fromISO(isoDate);
+  const currentTime = DateTime.fromJSDate(new Date());
+
+  const diff = currentTime.diff(oldTime, [
+    "years",
+    "months",
+    "days",
+    "hours",
+    "minutes",
+  ]);
+
+  let timeAgo = "";
+  // get the first non-zero time
+  for (const [key, value] of Object.entries(diff.values)) {
+    const roundedValue = Math.floor(value);
+
+    if (roundedValue > 1) {
+      timeAgo = `${roundedValue} ${key} ago`;
+      break;
+    } else if (roundedValue === 1) {
+      timeAgo = `${roundedValue} ${key.slice(0, -1)} ago`;
+      break;
+    } else {
+      timeAgo = "a few moments ago";
+    }
+  }
+
+  return timeAgo;
+};
