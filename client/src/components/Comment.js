7f0d1f154080843a7763601d77f8761a6d0fa6b7
Jason Kim <jason.s.kim95@gmail.com>
2022-09-19 15:34:16 -0400

    Add ability to delete comments

--- client/src/components/Comment.js
+++ client/src/components/Comment.js
@@ -1,17 +1,37 @@
-import { useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import { decodeHTML, getTimeAgo } from "../helpers/helper";
+import { ReactComponent as DeleteIcon } from "../assets/delete.svg";
 import "../styles/Comment.css";
+import { authContext } from "../contexts/AuthContext";
 
 const Comment = ({ comment }) => {
   const [user, setUser] = useState({});
+  const { auth } = useContext(authContext);
+
   useEffect(() => {
     fetch(`http://localhost:9000/users/user/${comment.author}`)
       .then((res) => res.json())
       .then((res) => setUser(res.user));
   }, []);
 
+  const deleteComment = (blog, comment) => {
+    fetch(`http://localhost:9000/blogs/blog/${blog}/comment/${comment}`, {
+      method: "DELETE",
+    }).catch((err) => console.err(err));
+  };
+
   return (
     <div className="comment">
+      {auth.data.user && auth.data.user._id === comment.author ? (
+        <DeleteIcon
+          className="delete-comment"
+          onClick={(event) => {
+            event.stopPropagation();
+            deleteComment(comment.blog, comment._id);
+            window.location.reload();
+          }}
+        />
+      ) : null}
       <div className="user">
         <img
           src={`http://localhost:9000/avatars/${user.avatar}`}
