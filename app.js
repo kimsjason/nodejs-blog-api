00197c95df0c718cfe37d49be47fb2184c8e9a8e
Jason Kim <jason.s.kim95@gmail.com>
2022-09-20 23:26:32 -0400

    Add api extension to backend routes

--- app.js
+++ app.js
@@ -79,9 +79,9 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use("/", indexRouter);
-app.use("/users", usersRouter);
-app.use("/blogs", blogsRouter);
+app.use("/api", indexRouter);
+app.use("/api/users", usersRouter);
+app.use("/api/blogs", blogsRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
